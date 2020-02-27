e9fc0f6dc103a1fa8b8f9bdc260e9e80734d24c4
RaveMaker <ravemaker@gmail.com>
2020-02-27 19:43:33 +0200

    fix some bash syntax

--- license-check.sh
+++ license-check.sh
@@ -6,15 +6,15 @@
 # by RaveMaker - http://ravemaker.net
 
 # Load settings
-if [ -f settings.cfg ] ; then
-    echo "Loading settings..."
-    source settings.cfg
+if [ -f settings.cfg ]; then
+  echo "Loading settings..."
+  source settings.cfg
 else
-    echo "ERROR: Create settings.cfg (from settings.cfg.example)"
-    exit
-fi;
+  echo "ERROR: Create settings.cfg (from settings.cfg.example)"
+  exit
+fi
 
-workdir=`pwd`
+workdir=$PWD
 logfile=$workdir/license-check.run
 listfile=$workdir/license-check.lst
 finallogfile=$workdir/license-check-$(date +%Y%m%d)
@@ -22,35 +22,34 @@ emailfile=$workdir/license-check.email
 
 (
 
-cat $listfile | while read line
-do
-	appname="$(echo $line | awk '{print $(NF-1)}')"
-	portserver="$(echo $line | awk '{print $NF}')"
-	/usr/bin/lmutil lmstat -a -c $portserver |grep Error
-	if [ $? != 1 ] ; then
-	    echo $appname license DOWN - $portserver >> $emailfile
-	    echo "================================================================================"
-	    echo $appname license DOWN - $portserver
-	    echo "================================================================================"
-	else
-	    echo "================================================================================"
-	    echo $appname license OK - $portserver
-	    echo "================================================================================"
-	fi
-done
-echo ""
-echo "**************************************"
-echo "*****           All Done         *****"
-echo "**************************************"
-echo ""
-) 2>&1 | tee -a $logfile
+  while read -r line; do
+    appname="$(echo "$line" | awk '{print $(NF-1)}')"
+    portserver="$(echo "$line" | awk '{print $NF}')"
+    /usr/bin/lmutil lmstat -a -c "$portserver" | grep Error
+    if [ $? != 1 ]; then
+      echo "$appname license DOWN - $portserver" >>"$emailfile"
+      echo "================================================================================"
+      echo "$appname license DOWN - $portserver"
+      echo "================================================================================"
+    else
+      echo "================================================================================"
+      echo "$appname license OK - $portserver"
+      echo "================================================================================"
+    fi
+  done <"$listfile"
+  echo ""
+  echo "**************************************"
+  echo "*****           All Done         *****"
+  echo "**************************************"
+  echo ""
+) 2>&1 | tee -a "$logfile"
 
-mv $logfile $finallogfile
+mv "$logfile" "$finallogfile"
 
-if [ -a $emailfile ] ; then 
-    echo "**************************************"
-    cat $emailfile
-    echo "**************************************"
-    cat $emailfile |mail -s "licenses check" $emailaddress
-    rm -f $emailfile
+if [ -a "$emailfile" ]; then
+  echo "**************************************"
+  cat "$emailfile"
+  echo "**************************************"
+  mail -s "licenses check" "$emailaddress" <"$emailfile"
+  rm -f "$emailfile"
 fi
